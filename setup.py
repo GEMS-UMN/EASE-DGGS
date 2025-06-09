b53fff221fad77c3707783486d8fcd1becb930fb
Jeff Thompson <jathomps@umn.edu>
2025-06-09 11:34:04 -0500

    PyPI changes

--- setup.py
+++ setup.py
@@ -1,23 +1,18 @@
 from setuptools import setup, find_packages
 
+from setuptools import setup, find_packages
+
 setup(
-    name="easedggs",
-    version='0.2.0',
+    name="ease-dggs",
+    version="0.2.0",
     description='EASE-DGGS Library Code and Tools',
     url='https://github.com/GEMS-UMN/EASE-DGGS',
     author='GEMS Geospatial Developers',
     author_email='gemssupport@umn.edu',
-    packages=find_packages(
-        where='src',
-        include=['easedggs*'],
-        exclude=['additional'],
-        ),
-    package_dir={"": "src"},
-    package_data={'': ['grid_spec.json', 'levels_specs.json']},
+    packages=find_packages(),  # No need for `where="src"` here
     include_package_data=True,
-    install_requires =[
+    install_requires=[
         'pytest',
-        #'python >= 3.9.7',
         'pyproj >= 3.2.1',
         'geopandas >= 0.10.0',
         'numpy >= 1.21.2',
@@ -25,7 +20,9 @@ setup(
         'shapely >= 1.7.1',
         'rasterio >= 1.2',
         ],
+    python_requires=">=3.9",
     classifiers=[
         'Programming Language :: Python :: 3.9',
         ]
-    )
+)
+
